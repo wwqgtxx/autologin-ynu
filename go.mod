0cdeb43d9be11661ec5bb7ec626b3299d6b9e03e
wwqgtxx <wwqgtxx@gmail.com>
2021-06-12 19:47:20 +0800

    use goja replace otto, do base64 and json marshal in golang native

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
-	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
-	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	github.com/dop251/goja v0.0.0-20210427212725-462d53687b0d
 )
--- portal_utils.go
+++ portal_utils.go
@@ -3,9 +3,17 @@ package main
 import (
 	"crypto"
 	"crypto/hmac"
+	"encoding/base64"
 	"encoding/hex"
-	"github.com/robertkrimen/otto"
+	"encoding/json"
+	"github.com/dop251/goja"
 )
+
+var vm *goja.Runtime
+var _xEncode goja.Callable
+var _str2Bytes goja.Callable
+var _ALPHA = "LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA"
+var _StdEncoding = base64.NewEncoding(_ALPHA).WithPadding('=')
 
 func pwd(passWord string, challenge string) string {
 	hmacMD5 := hmac.New(crypto.MD5.New, []byte(challenge))
@@ -19,100 +27,10 @@ func chksum(chkstr string) string {
 	return hex.EncodeToString(sha1.Sum(nil))
 }
 
-func info(userName string, passWord string, acID string, userIP string, challenge string) string {
-	vm := otto.New()
-	_, _ = vm.Run(`
-//jquery-base64 v1.0
+func init() {
+	vm = goja.New()
+	_, err := vm.RunString(`
 "use strict";
-base64 = (function($) {
-    var _PADCHAR = "="
-      , _ALPHA = "LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA"
-      , _VERSION = "1.0";
-    function _getbyte64(s, i) {
-        var idx = _ALPHA.indexOf(s.charAt(i));
-        if (idx === -1) {
-            throw "Cannot decode base64"
-        }
-        return idx
-    }
-    function _setAlpha(s) {
-        _ALPHA = s;
-    }
-    function _decode(s) {
-        var pads = 0, i, b10, imax = s.length, x = [];
-        s = String(s);
-        if (imax === 0) {
-            return s
-        }
-        if (imax % 4 !== 0) {
-            throw "Cannot decode base64"
-        }
-        if (s.charAt(imax - 1) === _PADCHAR) {
-            pads = 1;
-            if (s.charAt(imax - 2) === _PADCHAR) {
-                pads = 2
-            }
-            imax -= 4
-        }
-        for (i = 0; i < imax; i += 4) {
-            b10 = (_getbyte64(s, i) << 18) | (_getbyte64(s, i + 1) << 12) | (_getbyte64(s, i + 2) << 6) | _getbyte64(s, i + 3);
-            x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 255, b10 & 255))
-        }
-        switch (pads) {
-        case 1:
-            b10 = (_getbyte64(s, i) << 18) | (_getbyte64(s, i + 1) << 12) | (_getbyte64(s, i + 2) << 6);
-            x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 255));
-            break;
-        case 2:
-            b10 = (_getbyte64(s, i) << 18) | (_getbyte64(s, i + 1) << 12);
-            x.push(String.fromCharCode(b10 >> 16));
-            break
-        }
-        return x.join("")
-    }
-    function _getbyte(s, i) {
-        var x = s.charCodeAt(i);
-        if (x > 255) {
-            throw "INVALID_CHARACTER_ERR: DOM Exception 5"
-        }
-        return x
-    }
-    function _encode(s) {
-        if (arguments.length !== 1) {
-            throw "SyntaxError: exactly one argument required"
-        }
-        s = String(s);
-        var i, b10, x = [], imax = s.length - s.length % 3;
-        if (s.length === 0) {
-            return s
-        }
-        for (i = 0; i < imax; i += 3) {
-            b10 = (_getbyte(s, i) << 16) | (_getbyte(s, i + 1) << 8) | _getbyte(s, i + 2);
-            x.push(_ALPHA.charAt(b10 >> 18));
-            x.push(_ALPHA.charAt((b10 >> 12) & 63));
-            x.push(_ALPHA.charAt((b10 >> 6) & 63));
-            x.push(_ALPHA.charAt(b10 & 63))
-        }
-        switch (s.length - imax) {
-        case 1:
-            b10 = _getbyte(s, i) << 16;
-            x.push(_ALPHA.charAt(b10 >> 18) + _ALPHA.charAt((b10 >> 12) & 63) + _PADCHAR + _PADCHAR);
-            break;
-        case 2:
-            b10 = (_getbyte(s, i) << 16) | (_getbyte(s, i + 1) << 8);
-            x.push(_ALPHA.charAt(b10 >> 18) + _ALPHA.charAt((b10 >> 12) & 63) + _ALPHA.charAt((b10 >> 6) & 63) + _PADCHAR);
-            break
-        }
-        return x.join("")
-    }
-    return {
-        decode: _decode,
-        encode: _encode,
-        setAlpha: _setAlpha,
-        VERSION: _VERSION
-    }
-}());
-
 	function xEncode(str, key) {
 		if (str == "") {
             return "";
@@ -176,34 +94,65 @@ base64 = (function($) {
             return a.join('');
         }
     }
-    function json(d) {
-    	return JSON.stringify(d);
-    }
 
-    function info(d, k) {
-        return "{SRBX1}" + base64.encode(xEncode(json(d), k));
-    }
-
-	function info2(username,password,client_ip,ac_id,token){
-		var enc = "s" + "run" + "_bx1";
-		var i = info({
-        			username: username,
-        			password: password,
-        			ip: client_ip,
-        			acid: ac_id,
-        			enc_ver: "srun_bx1"
-        		}, token)
-		return i;
+	function str2bytes(s){
+		s = String(s);
+		var i,t = [];
+		for (i = 0; i < s.length; i++) {
+			var x = s.charCodeAt(i);
+			if (x > 255) {
+				throw "INVALID_CHARACTER_ERR: DOM Exception 5"
+			}
+            t.push(x);
+        }
+		return t;
 	}
 `)
-	info2, err := vm.Get("info2")
+	if err != nil {
+		Logger.Fatalln(err)
+	}
+
+	var ok bool
+	_xEncode, ok = goja.AssertFunction(vm.Get("xEncode"))
+	if !ok {
+		Logger.Fatalln("can't get xEncode function")
+	}
+	_str2Bytes, ok = goja.AssertFunction(vm.Get("str2bytes"))
+	if !ok {
+		Logger.Fatalln("can't get str2Bytes function")
+	}
+}
+
+func xEncode(d string, k string) (data []byte, err error) {
+	v1, err := _xEncode(goja.Undefined(), vm.ToValue(d), vm.ToValue(k))
+	if err != nil {
+		return
+	}
+	v, err := _str2Bytes(goja.Undefined(), v1)
+	if err != nil {
+		return
+	}
+	err = vm.ExportTo(v, &data)
+	return
+}
+
+func info(userName string, passWord string, acID string, userIP string, challenge string) string {
+	dict := map[string]string{
+		"username": userName,
+		"password": passWord,
+		"ip":       userIP,
+		"acid":     acID,
+		"enc_ver":  "srun_bx1",
+	}
+	jsonByte, err := json.Marshal(dict)
 	if err != nil {
 		Logger.Println(err)
 	}
-	v, err := info2.Call(otto.Value{}, userName, passWord, userIP, acID, challenge)
+
+	v, err := xEncode(string(jsonByte), challenge)
 	if err != nil {
 		Logger.Println(err)
 	}
 	//Logger.Println(v)
-	return v.String()
+	return "{SRBX1}" + _StdEncoding.EncodeToString(v)
 }
